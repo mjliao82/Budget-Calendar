54e7e421b9c551d310138b3b8ce416714fc29a91
Michael <mjliao@buffalo.edu>
2023-01-14 23:25:17 -0500

    type boxes

--- GUI.py
+++ GUI.py
@@ -3,6 +3,7 @@ from tkinter import *
 import main
 root = Tk()
 
+root.title("Budget Calendar")
 def jan1203():
     output = main.pulling("01/12/2023")
     result = Label(root, text=str(output))
@@ -10,10 +11,26 @@ def jan1203():
     return None
 
 #on GUI
-myLabel = Label(root, text="Budget Calendar")
+Initial=Label(root, text="Enter your transaction")
+space=Label(root, text="                     ")
 month=Label(root, text="January 2023")
-myButton=Button(root, text="12th", command=jan1203)
-myLabel.pack()
+twelve=Button(root, text="12th", command=jan1203)
+date=Entry(root, width=30)
+income=Entry(root, width=30)
+spent=Entry(root, width=30)
+
+
+
+
+
+Initial.pack()
+date.pack()
+date.insert(0, "ex: '01/12/2023'")
+income.pack()
+income.insert(1, "ex: '10'")
+spent.pack()
+spent.insert(2, "ex: '20'")
+space.pack()
 month.pack()
-myButton.pack()
+twelve.pack()
 root.mainloop()
