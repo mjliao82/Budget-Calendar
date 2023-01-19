b94daaaa4e2bd316a0758f25e85b08eabab1b04c
Michael <mjliao@buffalo.edu>
2023-01-19 16:34:01 -0500

    CALENDAR ON!!!

--- GUI.py
+++ GUI.py
@@ -1,20 +1,17 @@
 from tkinter import *
+from tkcalendar import Calendar
 
 import main
 root = Tk()
-
+root.geometry("500x500")
 root.title("Budget Calendar")
-def jan1203():
-    output = main.pulling("01/12/2023")
-    result = Label(root, text=str(output))
-    result.grid(row=8, column=0, columnspan=4)
-    return None
+
+cal = Calendar(root, font="Arial 16", selectmode='day', year=2023, month=1, day=19, foreground="BLACK", selectforeground="red")
 
 #on GUI
 Initial=Label(root, text="Enter your transaction")
 space=Label(root, text="                     ")
-month=Label(root, text="January 2023")
-twelve=Button(root, text="12th", command=jan1203)
+
 instruction1=Label(root, text="Enter date")
 date=Entry(root, width=15)
 instruction2=Label(root, text="Enter earnings")
@@ -45,9 +42,7 @@ instruction3.grid(row=3, column=0)
 #row4
 enter.grid(row=4, column=0, columnspan=4, padx=20, pady=20)
 #row5
-space.grid(row=5, column=0)
+cal.grid(row=5, column=0, columnspan=6)
 #row6
-month.grid(row=6, column=0, columnspan=4)
-twelve.grid(row=7, column=0, columnspan=4)
 space.grid(row=8, column=0, padx=40, pady=40)
 root.mainloop()
