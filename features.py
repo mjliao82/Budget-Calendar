13f53b898a2cef886c818f3ab67b0179e11f3788
Michael <mjliao@buffalo.edu>
2023-01-21 23:11:44 -0500

    Basic functionalities

---
+++ features.py
@@ -0,0 +1,14 @@
+import csv
+
+head={"date": "",
+      "housing": 0,
+      "transportations": 0,
+      "food": 0,
+      "utilities": 0,
+      "insurance": 0,
+      "med&healthcare": 0,
+      "others": 0
+      }
+
+
+
