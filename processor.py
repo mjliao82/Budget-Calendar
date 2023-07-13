c72dbf28a737541199bb2fab722dd26f1607ca91
Rchen <rchen72@buffalo.edu>
2023-07-13 16:14:43 -0400

    sample remove line

--- processor.py
+++ processor.py
@@ -1,5 +1,12 @@
 import csv
 
 
-
+def row_delete(filename, row_num):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        lines = list(reader)
+        del lines[row_num]
+    with open(filename, 'w', newline = '') as file:
+        writer = csv.writer(file)
+        writer.writerow(lines)
 
