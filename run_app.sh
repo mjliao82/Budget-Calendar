62d3b5f74172617c7265b50140d41e74a6dafbea
Michael <mjliao@buffalo.edu>
2023-07-17 15:25:49 +0800

    Mac Desktop Automation

---
+++ run_app.sh
@@ -0,0 +1,9 @@
+#This is Mike's Mac, yours will NOT work.
+#DO NOT run this, idk what will happen, most likily BAD things, VERY BAD.
+#!/bin/bash
+#!/bin/bash
+source /Users/mightymike/Budget-Calendar-1/.venv/bin/activate
+python /Users/mightymike/Budget-Calendar-1/app.py
+
+
+
